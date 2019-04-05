b31d68da47a490d2990cc63aefd8e47674cd7874
HAL9001 <daiwzero@gmail.com>
2019-04-06 01:11:59 +0800

    use org-re-reveal instead of ox-reveal

--- style/config.el
+++ style/config.el
@@ -1,4 +1,6 @@
 ;; Activate TOC progress plugin.
-(add-to-list 'org-reveal-external-plugins
+(add-to-list 'org-re-reveal-external-plugins
              (cons 'toc-progress
-                   " { src: '%s../Reveal.js-TOC-Progress/plugin/toc-progress/toc-progress.js', async: true, callback: function() { toc_progress.initialize('reduce', 'rgba(120,138,130,0.2)'); toc_progress.create(); } }"))
+                   " { src: \"%srelative../Reveal.js-TOC-Progress/plugin/toc-progress/toc-progress.js\", async: true, callback: function() { toc_progress.initialize('reduce', 'rgba(120,138,130,0.2)'); toc_progress.create(); } }"))
+
+(setq org-re-reveal-title-slide nil)

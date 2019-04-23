e36d40e175df76294ca1e479fecdd8718007cbc4
Wang-Zhou Dai <daiwzero@gmail.com>
2019-04-23 12:28:15 +0100

    use org-mode git version

--- style/config.el
+++ style/config.el
@@ -1,6 +1,4 @@
 ;; Activate TOC progress plugin.
-(add-to-list 'org-re-reveal-external-plugins
+(add-to-list 'org-reveal-external-plugins
              (cons 'toc-progress
                    " { src: \"%s../Reveal.js-TOC-Progress/plugin/toc-progress/toc-progress.js\", async: true, callback: function() { toc_progress.initialize('reduce', 'rgba(120,138,130,0.2)'); toc_progress.create(); } }"))
-
-(setq org-re-reveal-title-slide nil)
